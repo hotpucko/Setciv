3fdff2cdd86c70bda2aa975a3da9bd8161e11ea1
Phoqus <phoquslgen@gmail.com>
2016-07-23 20:50:41 +0200

    Add: TileInfo text script

---
+++ Assets/Scripts/GUI/GUIManager.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class GUIManager : MonoBehaviour {
+
+    public Text tileInfo;
+         
+    public void WriteTileInfo(string info) {
+        tileInfo.text = info;
+    }
+}
