312228537fa2b364f0e777f9bd12c688fdec6c54
Phoqus <phoquslgen@gmail.com>
2016-07-23 19:15:29 +0200

    Fixed: scroll direction Add: Direcional light in scene

--- Assets/Scripts/Camera/CameraControls.cs
+++ Assets/Scripts/Camera/CameraControls.cs
@@ -11,7 +11,7 @@ public class CameraControls : MonoBehaviour {
 
 
 	void Update () {
-		transform.position += new Vector3 (Input.GetAxis ("Horizontal"), Input.GetAxis("Mouse ScrollWheel") * 10 * Speed, Input.GetAxis ("Vertical") * Speed);
+		transform.position += new Vector3 (Input.GetAxis ("Horizontal"), Input.GetAxis("Mouse ScrollWheel") * -10 * Speed, Input.GetAxis ("Vertical") * Speed);
         transform.position = new Vector3(Mathf.Clamp(transform.position.x, 0, 100), Mathf.Clamp(transform.position.y, 5, 50), Mathf.Clamp(transform.position.z, -30, 70));
 		/*if (Input.GetKeyDown (KeyCode.E)) {
 			RaycastHit hit;
